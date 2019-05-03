d911372e4a397d939b977c316bfd2aedb247b3a1
Quoc Bui <quocbui@gmail.com>
2019-05-03 13:24:08 -0700

    removed .cache

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/quoc/Documents/Codesmith/15_Puzzle/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---src-pages-index-js": () => import("/Users/quoc/Documents/Codesmith/15_Puzzle/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
-}
-
-exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/quoc/Documents/Codesmith/15_Puzzle/.cache/data.json")
-
